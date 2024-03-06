93e237aa9668df43baf6dec75eee68643001638c
collinshepherd <77903396+collinshepherd@users.noreply.github.com>
2024-03-05 19:13:20 -0600

    Completed needed code for queries

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,12 +1,28 @@
 const { signToken, AuthenticationError } = require('../utils/auth')
 
-const { User } = require('../models/index')
+const { User, Account, Task } = require('../models/index')
 
 const resolvers = {
     Query: {
         Users: async () => {
             // Get and return all documents from the classes collection
             return await User.find({})
+        },
+        User: async (id) => {
+            return await User.findOne({ id })
+        },
+        Accounts: async () => {
+            return await Account.find({})
+        },
+        Account: async (id) => {
+            return await Account.findOne({ id })
+        },
+        Tasks: async () => {
+            // Get and return all documents from the classes collection
+            return await Task.find({})
+        },
+        Task: async (id) => {
+            return await Task.findOne({ id })
         },
     },
 }
