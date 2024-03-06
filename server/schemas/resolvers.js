257bb098e8fdcc9a33bbdd787c5b631b0ed96505
collinshepherd <77903396+collinshepherd@users.noreply.github.com>
2024-03-06 09:46:10 -0600

    Completed queries for all collections

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,8 @@
 const { signToken, AuthenticationError } = require('../utils/auth')
 
+const { mongoose } = require('mongoose')
+const ObjectId = mongoose.mongo.ObjectId
+
 const { User, Account, Task } = require('../models/index')
 
 const resolvers = {
@@ -8,21 +11,38 @@ const resolvers = {
             // Get and return all documents from the classes collection
             return await User.find({})
         },
-        User: async (id) => {
-            return await User.findOne({ id })
+        User: async (parent, id) => {
+            // Get and return a single user from the user collection
+            const user = await User.findOne({ _id: id })
+
+            return user
+        },
+        UserTasks: async (parent, id) => {
+            // Taking the passed id and creating that into a mongoose object id
+            const userId = new ObjectId(id._id)
+
+            // Finding all tasks the user has assigned to it
+            const userTasks = await Task.find({
+                assignedUser: userId,
+            })
+
+            return userTasks
         },
         Accounts: async () => {
+            // Get and return all documents from the account collection
             return await Account.find({})
         },
-        Account: async (id) => {
-            return await Account.findOne({ id })
+        Account: async (parent, { id }) => {
+            // Get and return a single account from the account collection
+            return await Account.findOne({ _id: id })
         },
         Tasks: async () => {
-            // Get and return all documents from the classes collection
+            // Get and return all documents from the Task collection
             return await Task.find({})
         },
-        Task: async (id) => {
-            return await Task.findOne({ id })
+        Task: async (parent, { id }) => {
+            // Get and return a single task from the task collection
+            return await Task.findOne({ _id: id })
         },
     },
 }
