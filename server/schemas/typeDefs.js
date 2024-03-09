859b74ed7341e1e68af7c75ef0f330eb2c3a58bc
collinshepherd <77903396+collinshepherd@users.noreply.github.com>
2024-03-09 13:05:49 -0600

    Conditional rendering for nav bar buttons

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,7 +5,7 @@ type User {
     password: String
     age: Int
     balance: Float
-    roles: [String]
+    role: String
     accountId: ID
   }
 
@@ -48,7 +48,7 @@ type Query {
 type Mutation {
     createAccount(familyName: String!, email: String!, password: String!): Auth
     accountLogin(email: String!, password: String!): Auth
-    createUser(name: String!, password: String!): User
+    createUser(name: String!, password: String!, role: String!): User
     userLogin(name: String!, password: String!): UserAuth
     addTask(taskName: String!, assignedUser: ID!): Task
   }`;
